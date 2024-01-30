742c663a8fd7c9c2773c786a7fc69f7bfdc7aeb0
sulirb <121161440+sulirb@users.noreply.github.com>
2024-01-30 16:44:21 +0100

    seo in index

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -38,7 +38,7 @@ function Footer() {
       <div className="footer__flex-b">
         <div>
           <p>
-            © Copyright 2023 &#x2013; Equipement-velo.fr &#x2013; Tous droits
+            © Copyright 2024 &#x2013; Equipement-velo.fr &#x2013; Tous droits
             réservés
           </p>
         </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,3 @@
-import { Helmet, HelmetProvider } from "react-helmet-async";
 import ArticlesGrid from "../components/home/articlesGrid";
 import Banner from "../components/home/banner";
 import HomeText from "../components/home/homeText";
@@ -6,44 +5,12 @@ import Intro from "../components/home/intro";
 
 function Home() {
   return (
-    <HelmetProvider>
-      <div className="home">
-        <Helmet>
-          <meta
-            name="description"
-            content="Découvrez les meilleurs conseils d'Équipement Vélo pour choisir les casques, lunettes, vêtements et chaussures de vélo adaptés à votre style et à vos besoins. Rendez votre expérience cycliste plus sûre et plus confortable avec nos recommandations expertes."
-          />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@EquipementVelo" />
-          <meta
-            name="twitter:title"
-            content="Équipement Vélo - Roulez avec style et en sécurité"
-          />
-          <meta
-            name="twitter:description"
-            content="Découvrez les meilleurs conseils d'Équipement Vélo pour choisir les casques, lunettes, vêtements et chaussures de vélo adaptés à votre style et à vos besoins. Rendez votre expérience cycliste plus sûre et plus confortable avec nos recommandations expertes."
-          />
-          <meta name="twitter:url" content="https://equipement-velo.fr" />
-          <meta name="twitter:image" content="/src/assets/logo.png" />
-          <meta property="og:site_name" content="Équipement Vélo" />
-          <meta property="og:type" content="website" />
-          <meta
-            name="og:title"
-            content="Équipement Vélo - Roulez avec style et en sécurité"
-          />
-          <meta
-            name="og:description"
-            content="Découvrez les meilleurs conseils d'Équipement Vélo pour choisir les casques, lunettes, vêtements et chaussures de vélo adaptés à votre style et à vos besoins. Rendez votre expérience cycliste plus sûre et plus confortable avec nos recommandations expertes."
-          />
-          <meta name="og:url" content="https://equipement-velo.fr" />
-          <meta name="og:image" content="/src/assets/logo.png" />
-        </Helmet>
-        <Banner />
-        <Intro />
-        <ArticlesGrid />
-        <HomeText />
-      </div>
-    </HelmetProvider>
+    <div className="home">
+      <Banner />
+      <Intro />
+      <ArticlesGrid />
+      <HomeText />
+    </div>
   );
 }
 
